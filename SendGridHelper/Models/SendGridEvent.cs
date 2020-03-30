b59c61407d3f47b948ef0544ce26d1182d2ab96d
marcelo marques <marceloninho@gmail.com>
2020-03-29 21:22:46 -0300

    Webhook Events

--- SendGridHelper/Models/SendGridEvent.cs
+++ SendGridHelper/Models/SendGridEvent.cs
@@ -5,19 +5,31 @@ using System.Text;
 
 namespace SendGridHelper.Models {
     public class SendGridEvents {
-        public string email { get; set; }
-        public int timestamp { get; set; }
-        public int uid { get; set; } 
-        public int id { get; set; }
-        public string sendgrid_event_id { get; set; }
-        public string sg_message_id { get; set; }
-        [JsonProperty("event")] // event is protected keyword
-        public string sendgrid_event { get; set; }
-        public string type { get; set; }
-        public string reason { get; set; }
-        public string status { get; set; }
-        public string url { get; set; }
-        public string useragent { get; set; }
-        public string ip { get; set; }
+        [JsonProperty("email")]
+        public string Email { get; set; }
+        [JsonProperty("timestamp")]
+        public int Timestamp { get; set; }
+        [JsonProperty("uid")]
+        public int UID { get; set; }
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("sendgrid_event_id")]
+        public string SendGridEventId { get; set; }
+        [JsonProperty("sg_message_id")]
+        public string SGMessageId { get; set; }
+        [JsonProperty("sendgrid_event")] 
+        public string SendGridEvent { get; set; }
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        [JsonProperty("reason")]
+        public string Reason { get; set; }
+        [JsonProperty("status")]
+        public string Status { get; set; }
+        [JsonProperty("url")]
+        public string URL { get; set; }
+        [JsonProperty("useragent")]
+        public string UserAgent { get; set; }
+        [JsonProperty("ip")]
+        public string IP { get; set; }
     }
 }
---
+++ SendGridHelper/Models/SendGridWebHookEvent.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SendGridHelper.Models {
+    public class SendGridWebHookEvent {
+        [JsonProperty("email")] 
+        public string Email { get; set; }
+        [JsonProperty("timestamp")] 
+        public int Timestamp { get; set; }
+        [JsonProperty("__invalid_name__smtp-id")] 
+        public string InvalidNameSMTPId { get; set; }
+        [JsonProperty("event")] 
+        public string SendGridEvent { get; set; }
+        [JsonProperty("category")] 
+        public string Category { get; set; }
+        [JsonProperty("sg_event_id")] 
+        public string SGEventId { get; set; }
+        [JsonProperty("sg_message_id")] 
+        public string SGMessageId { get; set; }
+        [JsonProperty("response")] 
+        public string Response { get; set; }
+        [JsonProperty("attempt")] 
+        public string Attempt { get; set; }
+        [JsonProperty("useragent")] 
+        public string Useragent { get; set; }
+        [JsonProperty("ip")] 
+        public string IP { get; set; }
+        [JsonProperty("url")] 
+        public string URL { get; set; }
+        [JsonProperty("reason")] 
+        public string Reason { get; set; }
+        [JsonProperty("status")] 
+        public string Status { get; set; }
+        [JsonProperty("asm_group_id")] 
+        public int? ASMGroupId { get; set; }
+    }
+}
